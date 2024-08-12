8970cebaba4d54d1148ce44f5b85f73d942881c7
gia-grigalashvili <grigalagia07@gmail.com>
2024-08-12 15:07:06 +0400

    addtodo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState([]);
+
+  const addTodo = (event) => {
+    const inputElement = event.target as HTMLInputElement;
+    if (event.key === "Enter" && inputElement.value.trim()) {
+      setTask([
+        ...task,
+        { id: Math.random(), title: inputElement.value, isDone: false },
+      ]);
+      inputElement.value = "";
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" onKeyDown={addTodo} />
+      <ul>
+        {task.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
